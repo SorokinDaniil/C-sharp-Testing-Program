a01963ac79b2670c8867971eba1a5b063be41f8f
JasonJson <daniil.sorokin.228@gmail.com>
2019-04-23 19:04:02 +0300

    Fixed style

--- NavigationDrawer-master/NavigationDrawerPopUpMenu2/Window1.xaml.cs
+++ NavigationDrawer-master/NavigationDrawerPopUpMenu2/Window1.xaml.cs
@@ -12,9 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using MaterialDesignThemes.Wpf;
+using NUnit.Framework;
 
 namespace TestingProgram
 {
+     [TestFixture]
     /// <summary>
     /// Логика взаимодействия для Window1.xaml
     /// </summary>
@@ -23,8 +25,27 @@ namespace TestingProgram
         public Window1()
         {
           InitializeComponent();
-          HeadLabelName.Children.Add(new UserLabelName());
-         LeftPanel.Children.Add(new UserLeftPanel());
+          HeadLabelName.Children.Add(new AdminLabelName());
+          LeftPanel.Children.Add(new AdminLeftPanel());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Head.Visibility = Visibility.Hidden;
+            //StartPanel.Visibility = Visibility.Hidden;
+        }
+
+        private void Sample1_DialogHost_OnDialogClosing(object sender, DialogClosingEventArgs eventArgs)
+        {
+            Console.WriteLine("SAMPLE 1: Closing dialog with parameter: " + (eventArgs.Parameter ?? ""));
+
+            //you can cancel the dialog close:
+            //eventArgs.Cancel();
+
+            if (!Equals(eventArgs.Parameter, true)) return;
+
+            if (!string.IsNullOrWhiteSpace(FruitTextBox.Text))
+                FruitListBox.Items.Add(FruitTextBox.Text.Trim());
         }
     }
 }
